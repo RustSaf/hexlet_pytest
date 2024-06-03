95ea50aeb89b21959c4719b0d73f57e67a616a60
Rust <afarg1710@gmail.com>
2024-06-03 18:39:35 +0500

    Test stack

---
+++ tests/test_stack.py
@@ -0,0 +1,14 @@
+import hexlet_pytest.stack_with_tests
+
+
+hexlet_pytest.stack_with_tests.test_pop_with_empty_stack()
+
+
+hexlet_pytest.stack_with_tests.test_stack1()
+
+
+hexlet_pytest.stack_with_tests.test_stack2()
+
+
+hexlet_pytest.stack_with_tests.test_emptiness()
+
